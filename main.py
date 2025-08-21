c81810b963bd022a0c45aba65e69b8d638765f89
Benjamin <benjamon2301@gmail.com>
2025-08-21 18:20:45 -0400

    llll

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 print("Hola Mundo")
 print("xie xie")
 print("mish")
-print("uwu")
+
 print("kawaii")
 
-#soy god
+
 
 print("holi")
 print("anyonjaseio")
-#ola ola
 
-#creo que ya se arreglo  
 print("hi")
 
 print("hola")
 
-#aaaaa1aaaa
+
 
 
 print("hola")
